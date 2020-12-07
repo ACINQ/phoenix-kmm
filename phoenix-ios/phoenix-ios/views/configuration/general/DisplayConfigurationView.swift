05827f4a6bc6244accce5e2767ff0c03fe19d995
Robbie Hanson <robbiehanson@deusty.com>
2020-12-07 16:34:42 -0500

    Removing dead code

--- phoenix-ios/phoenix-ios/views/configuration/general/DisplayConfigurationView.swift
+++ phoenix-ios/phoenix-ios/views/configuration/general/DisplayConfigurationView.swift
@@ -1,64 +1,58 @@
 import SwiftUI
 import PhoenixShared
 
+
+
 struct DisplayConfigurationView: View {
-
+	
 	var body: some View {
-		MVIView({ $0.displayConfiguration() }) { model, postIntent in
-			Form {
-				Section {
-					Picker(
-						selection: Binding(
-							get: { Prefs.shared.fiatCurrency },
-							set: {
-								Prefs.shared.fiatCurrency = $0
-							}
-						), label: Text("Fiat currency")
-					) {
-						ForEach(0 ..< FiatCurrency.default().values.count) {
-							let fiatCurrency = FiatCurrency.default().values[$0]
-							fiatCurrencyText(fiatCurrency).tag(fiatCurrency)
-						}
+		Form {
+			Section {
+				Picker(
+					selection: Binding(
+						get: { Prefs.shared.fiatCurrency },
+						set: { Prefs.shared.fiatCurrency = $0 }
+					), label: Text("Fiat currency")
+				) {
+					ForEach(0 ..< FiatCurrency.default().values.count) {
+						let fiatCurrency = FiatCurrency.default().values[$0]
+						fiatCurrencyText(fiatCurrency).tag(fiatCurrency)
 					}
-					
-					Picker(
-						selection: Binding(
-							get: { Prefs.shared.bitcoinUnit },
-							set: {
-								Prefs.shared.bitcoinUnit = $0
-							}
-						), label: Text("Bitcoin unit")
-					) {
-						ForEach(0 ..< BitcoinUnit.default().values.count) {
-							let bitcoinUnit = BitcoinUnit.default().values[$0]
-							bitcoinUnitText(bitcoinUnit).tag(bitcoinUnit)
-						}
+				}
+				
+				Picker(
+					selection: Binding(
+						get: { Prefs.shared.bitcoinUnit },
+						set: { Prefs.shared.bitcoinUnit = $0 }
+					), label: Text("Bitcoin unit")
+				) {
+					ForEach(0 ..< BitcoinUnit.default().values.count) {
+						let bitcoinUnit = BitcoinUnit.default().values[$0]
+						bitcoinUnitText(bitcoinUnit).tag(bitcoinUnit)
 					}
-                }
-
-				Section {
-					Picker(
-						selection: Binding(
-							get: { model.appTheme },
-							set: {
-								postIntent(DisplayConfiguration.IntentUpdateAppTheme(appTheme: $0))
-							}
-						), label: Text("application theme")
-					) {
-						ForEach(0..<AppTheme.default().values.count) {
-							let theme = AppTheme.default().values[$0]
-							Text(theme.label).tag(theme)
-						}
-					}.pickerStyle(SegmentedPickerStyle())
 				}
 			}
-			.frame(maxWidth: .infinity, maxHeight: .infinity)
-			.edgesIgnoringSafeArea(.bottom)
-			.navigationBarTitle("Display options", displayMode: .inline)
+			
+			Section {
+				Picker(
+					selection: Binding(
+						get: { Prefs.shared.theme },
+						set: { Prefs.shared.theme = $0 }
+					), label: Text("App theme")
+				) {
+					ForEach(0 ..< Theme.allCases.count) {
+						let theme = Theme.allCases[$0]
+						Text(theme.localized()).tag(theme)
+					}
+				}.pickerStyle(SegmentedPickerStyle())
+			}
+		}
+		.frame(maxWidth: .infinity, maxHeight: .infinity)
+		.edgesIgnoringSafeArea(.bottom)
+		.navigationBarTitle("Display options", displayMode: .inline)
 
-            Spacer()
-        }
-    }
+		Spacer()
+	}
 	
 	@ViewBuilder func fiatCurrencyText(_ fiatCurrency: FiatCurrency) -> some View {
 		
@@ -83,20 +77,8 @@ struct DisplayConfigurationView: View {
 
 class DisplayConfigurationView_Previews: PreviewProvider {
 
-	static let mockModel = DisplayConfiguration.Model(
-		fiatCurrency: .usd,
-		bitcoinUnit: .satoshi,
-		appTheme: .system
-	)
-
 	static var previews: some View {
-		mockView(DisplayConfigurationView())
+		DisplayConfigurationView()
 			.previewDevice("iPhone 11")
 	}
-
-    #if DEBUG
-    @objc class func injected() {
-        UIApplication.shared.windows.first?.rootViewController = UIHostingController(rootView: previews)
-    }
-    #endif
 }
--- phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/PhoenixBusiness.kt
+++ phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/PhoenixBusiness.kt
@@ -212,9 +212,6 @@ class PhoenixBusiness(private val ctx: PlatformContext) {
         override fun configuration(): ConfigurationController =
             AppConfigurationController(loggerFactory, walletManager)
 
-        override fun displayConfiguration(): DisplayConfigurationController =
-            AppDisplayConfigurationController(loggerFactory, appConfigurationManager)
-
         override fun electrumConfiguration(): ElectrumConfigurationController =
             AppElectrumConfigurationController(loggerFactory, appConfigurationManager, chain, masterPubkeyPath, walletManager, electrumClient)
 
