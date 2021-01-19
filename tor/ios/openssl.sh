9e83a77cd9f5e774f44d40f0a2e8a38077254207
Salomon BRYS <salomon.brys@gmail.com>
2021-01-19 17:14:41 +0100

    We do not use universal binaries

---
+++ tor/ios/openssl.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+[[ -z "$ARCH" ]] && echo "Please set the ARCH variable" && exit 1
+
+# Compiler options
+OPT_FLAGS="-O3 -g3 -fembed-bitcode"
+MAKE_JOBS=8
+MIN_IOS_VERSION=12.0
+
+if [ "$ARCH" == "arm64" ]; then
+  SDK="iphoneos"
+  HOST_FLAGS="-arch arm64 -arch arm64e -miphoneos-version-min=${MIN_IOS_VERSION} -isysroot $(xcrun --sdk ${SDK} --show-sdk-path)"
+  CONFIG="no-async zlib-dynamic ios64-xcrun"
+elif [ "$ARCH" == "x86_64" ]; then
+  SDK="iphonesimulator"
+  HOST_FLAGS="-arch x86_64 -mios-simulator-version-min=${MIN_IOS_VERSION} -isysroot $(xcrun --sdk ${SDK} --show-sdk-path)"
+  CONFIG="no-asm iossimulator-xcrun"
+else
+  echo "Unsupported ARCH: $ARCH"
+  exit 1
+fi
+
+# Locations
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+PREFIX="${SCRIPT_DIR}/build/${ARCH}"
+
+cd ../libs/openssl
+
+# Ensure -fembed-bitcode builds, as workaround for libtool macOS bug
+export MACOSX_DEPLOYMENT_TARGET="10.4"
+# Get the correct toolchain for target platforms
+CC=$(xcrun --find --sdk "${SDK}" clang)
+export CC
+export CFLAGS="${HOST_FLAGS} ${OPT_FLAGS}"
+export LDFLAGS="${HOST_FLAGS}"
+
+./Configure \
+  no-shared \
+  --prefix="${PREFIX}" \
+  enable-ec_nistp_64_gcc_128 \
+  $CONFIG
+
+make clean
+make depend
+mkdir -p "${PREFIX}" &> /dev/null
+make -j"${MAKE_JOBS}" build_libs
+make install_dev
+make distclean
---
+++ tor/ios/tor_in_thread.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+[[ -z "$ARCH" ]] && echo "Please set the ARCH variable" && exit 1
+
+# Compiler options
+OPT_FLAGS="-O3 -g3 -fembed-bitcode"
+MIN_IOS_VERSION=12.0
+
+if [ "$ARCH" == "arm64" ]; then
+  SDK="iphoneos"
+  HOST_FLAGS="-arch arm64 -arch arm64e -miphoneos-version-min=${MIN_IOS_VERSION} -isysroot $(xcrun --sdk ${SDK} --show-sdk-path)"
+  CHOST="arm-apple-darwin"
+elif [ "$ARCH" == "x86_64" ]; then
+  SDK="iphonesimulator"
+  HOST_FLAGS="-arch x86_64 -mios-simulator-version-min=${MIN_IOS_VERSION} -isysroot $(xcrun --sdk ${SDK} --show-sdk-path)"
+  CHOST="x86_64-apple-darwin"
+else
+  echo "Unsupported ARCH: $ARCH"
+  exit 1
+fi
+
+# Locations
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+PREFIX="${SCRIPT_DIR}/build/${ARCH}"
+
+cd ../tor_in_thread
+
+CC=$(xcrun --find --sdk "${SDK}" clang)
+
+mkdir -p build &> /dev/null
+echo $CC ${HOST_FLAGS} ${OPT_FLAGS} -I"${PREFIX}/include" -c -o build/tor_in_thread.o tor_in_thread.c
+$CC ${HOST_FLAGS} ${OPT_FLAGS} -I"${PREFIX}/include" -c -o build/tor_in_thread.o tor_in_thread.c
+
+mkdir -p "${PREFIX}/lib" &> /dev/null
+rm -f "${PREFIX}/lib/libtor_in_thread.a"
+ar r "${PREFIX}/lib/libtor_in_thread.a" "build/tor_in_thread.o"
+ranlib -c "${PREFIX}/lib/libtor_in_thread.a"
+
+mkdir -p "${PREFIX}/include" &> /dev/null
+cp -v tor_in_thread.h "${PREFIX}/include"
