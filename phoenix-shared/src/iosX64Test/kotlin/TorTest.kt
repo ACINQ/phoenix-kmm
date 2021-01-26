7f7fdd0f38e54509bf468d5b54eac849d9c0d961
Salomon BRYS <salomon.brys@gmail.com>
2021-01-26 12:10:38 +0100

    Socks5 without auth

--- phoenix-shared/src/iosX64Test/kotlin/TorTest.kt
+++ phoenix-shared/src/iosX64Test/kotlin/TorTest.kt
@@ -1,34 +1,43 @@
 package fr.acinq.phoenix.tor
 
+import fr.acinq.eclair.io.TcpSocket
 import fr.acinq.eclair.tests.utils.runSuspendBlocking
 import fr.acinq.phoenix.utils.PlatformContext
+import io.ktor.util.*
 import kotlinx.coroutines.delay
 import org.kodein.log.LoggerFactory
 import org.kodein.log.frontend.simplePrintFrontend
 import kotlin.test.Test
 import kotlin.time.ExperimentalTime
-import kotlin.time.days
+import kotlin.time.seconds
 
 
 class TorLocalTest {
+    @InternalAPI
     @OptIn(ExperimentalTime::class)
-    @Test fun run() = runSuspendBlocking {
+    @Test
+    fun run() = runSuspendBlocking {
         val tor = Tor(PlatformContext(), LoggerFactory(simplePrintFrontend))
 
         tor.start(this)
 
-        delay(1.days)
+        delay(2.seconds)
 
-//        val socket = TcpSocket.Builder().connect("neverssl.com", 80)
-//        socket.send("GET / HTTP/1.0\nhost: neverssl.com\n\n".encodeToByteArray())
-//
-//        val buffer = ByteArray(64 * 1024)
-//        try {
-//            while (true) {
-//                val read = socket.receiveAvailable(buffer)
-//                print(buffer.decodeToString(0, read))
-//            }
-//        } catch (_: TcpSocket.IOException) {}
-//        println()
+        val socket = TcpSocket.Builder().connectThroughSocks5(
+            "127.0.0.1", 34781,
+            "neverssl.com", 80
+        )
+        socket.send("GET / HTTP/1.0\nhost: neverssl.com\n\n".encodeToByteArray())
+
+        val buffer = ByteArray(64 * 1024)
+        try {
+            while (true) {
+                val read = socket.receiveAvailable(buffer)
+                print(buffer.decodeToString(0, read))
+            }
+        } catch (_: TcpSocket.IOException) {
+        }
+
+        tor.stop()
     }
 }
