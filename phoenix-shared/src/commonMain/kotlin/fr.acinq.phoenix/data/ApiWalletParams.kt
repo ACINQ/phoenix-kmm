33c858aba01d248b496d2171c9ff4d342d3243f7
dpad85 <5765435+dpad85@users.noreply.github.com>
2021-03-19 15:09:44 +0100

    Add query to simultaneously add and receving an incoming payment
    Also fix tests and the wallet params api object.

--- phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/data/ApiWalletParams.kt
+++ phoenix-shared/src/commonMain/kotlin/fr.acinq.phoenix/data/ApiWalletParams.kt
@@ -19,44 +19,45 @@ object ApiWalletParams {
         } else {
             testnet.export()
         }
-    }
 
-    @Serializable
-    data class ChainParams(
-        val version: Int,
-        @SerialName("latest_critical_version") val latestCriticalVersion: Int,
-        val trampoline: TrampolineParams,
-    ) {
-        fun export(): WalletParams = trampoline.v2.run {
-            WalletParams(nodes.first().export(), attempts.map { it.export() }, InvoiceDefaultRoutingFees(1000.msat, 100, CltvExpiryDelta(144)))
-        }
-    }
 
-    @Serializable
-    data class TrampolineParams(val v2: V2) {
         @Serializable
-        data class TrampolineFees(
-            @SerialName("fee_base_sat") val feeBaseSat: Long,
-            @SerialName("fee_per_millionths") val feePerMillionths: Long,
-            @SerialName("cltv_expiry") val cltvExpiry: Int,
+        data class ChainParams(
+            val version: Int,
+            @SerialName("latest_critical_version") val latestCriticalVersion: Int,
+            val trampoline: TrampolineParams,
         ) {
-            fun export(): fr.acinq.eclair.TrampolineFees = fr.acinq.eclair.TrampolineFees(feeBaseSat.sat, feePerMillionths, CltvExpiryDelta(cltvExpiry))
-        }
-
-        @Serializable
-        data class NodeUri(val name: String, val uri: String) {
-            fun export(): fr.acinq.eclair.NodeUri {
-                val parts = uri.split("@", ":")
-
-                val publicKey = PublicKey.fromHex(parts[0])
-                val host = parts[1]
-                val port = parts[2].toInt()
-
-                return fr.acinq.eclair.NodeUri(publicKey, host, port)
+            fun export(): WalletParams = trampoline.v2.run {
+                WalletParams(nodes.first().export(), attempts.map { it.export() }, InvoiceDefaultRoutingFees(1000.msat, 100, CltvExpiryDelta(144)))
             }
         }
 
         @Serializable
-        data class V2(val attempts: List<TrampolineFees>, val nodes: List<NodeUri> = emptyList())
+        data class TrampolineParams(val v2: V2) {
+            @Serializable
+            data class TrampolineFees(
+                @SerialName("fee_base_sat") val feeBaseSat: Long,
+                @SerialName("fee_per_millionths") val feePerMillionths: Long,
+                @SerialName("cltv_expiry") val cltvExpiry: Int,
+            ) {
+                fun export(): fr.acinq.eclair.TrampolineFees = fr.acinq.eclair.TrampolineFees(feeBaseSat.sat, feePerMillionths, CltvExpiryDelta(cltvExpiry))
+            }
+
+            @Serializable
+            data class NodeUri(val name: String, val uri: String) {
+                fun export(): fr.acinq.eclair.NodeUri {
+                    val parts = uri.split("@", ":")
+
+                    val publicKey = PublicKey.fromHex(parts[0])
+                    val host = parts[1]
+                    val port = parts[2].toInt()
+
+                    return fr.acinq.eclair.NodeUri(publicKey, host, port)
+                }
+            }
+
+            @Serializable
+            data class V2(val attempts: List<TrampolineFees>, val nodes: List<NodeUri> = emptyList())
+        }
     }
 }
